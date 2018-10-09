8e4967bd58fe4c16be47918eef1bd80d21e89104
Ghasem Kiani <ghasemkiani@gmail.com>
2018-10-09 14:13:00 +0330

    Added storable

---
+++ storable.js
@@ -0,0 +1,47 @@
+//	@ghasemkiani/baseapp/storable
+
+const {cutil} = require("@ghasemkiani/commonbase/cutil");
+
+const storable = {
+	_storeFile: null,
+	_store: null,
+	defaultStore: {},
+	get storeFile() {
+		if(!this._storeFile) {
+			this._storeFile = path.join(path.dirname(module.filename), "data.json");
+		}
+		return this._storeFile;
+	},
+	set storeFile(storeFile) {
+		this._storeFile = storeFile;
+	},
+	get store() {
+		if(!this._store) {
+			this.readStore();
+			process.on("exit", () => this.writeStore());
+		}
+		return this._store;
+	},
+	set store(store) {
+		this._store = store;
+	},
+	readStore() {
+		try {
+			this._store = JSON.parse(fs.readFileSync(this.storeFile, {encoding: "UTF-8"}));
+		} catch(e) {
+			console.log("Error in readStore:\n" + e.message);
+		}
+		if(cutil.isNil(this._store)) {
+			this._store = this.defaultStore;
+		}
+	},
+	writeStore() {
+		try {
+			fs.writeFileSync(this.storeFile, JSON.stringify(this._store), {encoding: "UTF-8"});
+		} catch(e) {
+			console.log("Error in writeStore:\n" + e.message);
+		}
+	},
+};
+
+module.exports = {storable};
