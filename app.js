5eae535a3196c34fae3025e74a923d6fc8d7533d
Ghasem Kiani <ghasemkiani@gmail.com>
2021-03-30 19:59:25 +0430

    fix commander update changes

--- app.js
+++ app.js
@@ -2,29 +2,30 @@
 
 const path = require("path");
 const Preferences = require("preferences");
-const commander = require("commander");
+const {Command} = require("commander");
 const {Base} = require("@ghasemkiani/commonbase/base");
 const {cutil} = require("@ghasemkiani/commonbase/cutil");
 const {irunner} = require("@ghasemkiani/commonbase/util/runner");
 
-class App extends cutil.mixin(Base, irunner) {}
-cutil.extend(App.prototype, {
-	commander: commander,
-	prefsId: "app.temp",
-	prefsEncrypt: false,
-	prefsFormat: "json",
-	useLocalPrefsFile: false,
-	_prefsFile: null,
+class App extends cutil.mixin(Base, irunner) {
+	get commander() {
+		if(!this._commander) {
+			this._commander = new Command();
+		}
+		return this._commander;
+	}
+	set commander(commander) {
+		this._commander = commander;
+	}
 	get prefsFile() {
 		if(!this._prefsFile && this.useLocalPrefsFile) {
 			this._prefsFile = path.join(path.dirname(module.filename), this.prefsId + ".prefs.json");
 		}
 		return this._prefsFile;
-	},
+	}
 	set prefsFile(prefsFile) {
 		this._prefsFile = prefsFile;
-	},
-	_prefs: null,
+	}
 	get prefs() {
 		if (!this._prefs) {
 			let options = {
@@ -37,35 +38,45 @@ cutil.extend(App.prototype, {
 			this._prefs = new Preferences(this.prefsId, this.defaultPrefs, options);
 		}
 		return this._prefs;
-	},
+	}
 	set prefs(prefs) {
 		this._prefs = prefs;
-	},
-	defaultPrefs: {},
+	}
 	parseInitOptions() {
 		this.commander.parse(process.argv);
-	},
+	}
 	defineInitOptions() {
 		this.commander.option("--prefs <prefs>", "The path to the preferences file");
-	},
+	}
 	async toApplyInitOptions() {
-		if(!cutil.isNil(this.commander.prefs)) {
-			this.prefsFile = this.commander.prefs;
+		let opts = this.commander.opts();
+		if(!cutil.isNil(opts.prefs)) {
+			this.prefsFile = opts.prefs;
 		}
-	},
+	}
 	async toAskInitOptions() {
 		//
-	},
+	}
 	async toDoStart() {
 		this.defineInitOptions();
 		this.parseInitOptions();
 		await this.toApplyInitOptions();
 		await this.toAskInitOptions();
 		await this.toRun();
-	},
+	}
 	async toRun() {
 		//
-	},
+	}
+}
+cutil.extend(App.prototype, {
+	_commander: null,
+	prefsId: "app.temp",
+	prefsEncrypt: false,
+	prefsFormat: "json",
+	useLocalPrefsFile: false,
+	_prefsFile: null,
+	_prefs: null,
+	defaultPrefs: {},
 });
 
 module.exports = {App};
